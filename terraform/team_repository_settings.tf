040adb8740377bdad1b072bf398babcf3363df06
Francisco Roa Prieto <franciscoroaprieto@gmail.com>
2024-02-01 15:05:56 +0100

    format terraform

--- terraform/team_repository_settings.tf
+++ terraform/team_repository_settings.tf
@@ -4,7 +4,7 @@ resource "github_team_repository" "voxsmart_team_repo" {
   ]
   for_each = var.team_repository_settings
 
-  team_id = each.value.team_id
+  team_id    = each.value.team_id
   repository = each.value.repository
   permission = each.value.permission
 }
